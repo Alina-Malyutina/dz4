83519e8a684a2eb5d1d7a2d3f82f2e47bcb9f6b7
Alina Malyutina <linamalutina1@gmail.com>
2022-11-12 10:47:40 +0300

    add task 27 - sum of numbers in program

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿int SumNumbers(int arg1)
+{
+    arg1 = Math.Abs(arg1);
+    string strArg1 = arg1.ToString();
+    int sum = 0;
+    for(int i=0; i<strArg1.Length; i++)
+    {
+        int num = Convert.ToInt32(strArg1[i].ToString());
+        sum = sum + num;
+    }
+    return sum;
+}
+
+Console.WriteLine("This program show sum of digits in your number");
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summ = SumNumbers(number);
+Console.WriteLine(summ);
