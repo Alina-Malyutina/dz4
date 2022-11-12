d6e20c181df030cb53a99b2a4888ee43de5be872
Alina Malyutina <linamalutina1@gmail.com>
2022-11-12 11:26:15 +0300

    add task 29 - array of 8 elements filling by user

---
+++ task29/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillArray(int [] numbers)
+{
+    
+    Console.WriteLine("This program assepts 8 numbers and made array of them");
+    Console.Write("Enter first number: ");
+    numbers[0] = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        Console.Write("Enter next number: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int [] numbers = new int [8];
+FillArray(numbers);
+
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]}, ");
+}
+Console.Write("]");
